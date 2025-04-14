c27c639024c963b08de70d3739ff705b565cbc7c
F-L-X-S <felixschuelke@icloud.com>
2025-04-14 19:09:34 +0200

    Added Setup for CMake and GTest

---
+++ tests/power_trigger_test.cc
@@ -0,0 +1,7 @@
+#include <power_trigger/power_trigger.h>
+#include <gtest/gtest.h>
+ 
+// Test, if the PowerTrigger keeps the trigger as long as defined
+TEST(PowerTriggerTest, HoldsTriggerWindow) {
+
+} 
