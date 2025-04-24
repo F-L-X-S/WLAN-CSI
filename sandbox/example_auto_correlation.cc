b8008e1ba6dd3cdb9f5af06be43a1075ba6eef75
F-L-X-S <felixschuelke@icloud.com>
2025-04-24 14:50:35 +0200

    Separated signal_generation

--- sandbox/example_auto_correlation.cc
+++ sandbox/example_auto_correlation.cc
@@ -1,40 +1,9 @@
 #include <iostream>
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <complex.h>
+#include <cmath>
+#include <complex>
 #include <liquid/liquid.h>
 #include <correlation/auto_corr.h>
-
-// Generate a sequence of samples, containing a pattern of the length `sequence_len`, 
-// that is repeated 'seq_repition' times and surrounded by zeros. The pattern starts at 'seq_start'. The sequence is padded with noise. 
-// https://github.com/jgaeddert/liquid-dsp/blob/master/examples/autocorr_cccf_example.c
-void GenerateSequence(unsigned int sequence_len, unsigned int seq_repition, unsigned int seq_start, std::complex<float>* x, unsigned int num_samples) {
-    assert(num_samples >= sequence_len * seq_repition);
-    float SNRdB=20.0f;                             // signal-to-noise ratio (dB)
-    std::complex<float> sequence[sequence_len];    // short sequence
-
-    // generate random training sequence using QPSK symbols
-    modemcf mod = modemcf_create(LIQUID_MODEM_QPSK);
-    for (unsigned int i=0; i<sequence_len; i++)
-        modemcf_modulate(mod, rand()%4, &sequence[i]);
-    modemcf_destroy(mod);
-
-    // write training sequence 'seq_repition' times in the middle of the array
-    unsigned int t=seq_start;
-    for (unsigned int i=0; i<seq_repition; i++) {
-        // copy sequence
-        memmove(&x[t], sequence, sequence_len*sizeof(std::complex<float>));
-        t += sequence_len;
-    }
-
-    // add noise
-    float nstd = powf(10.0f, -SNRdB/20.0f);
-    for (unsigned int i=0; i<num_samples; i++)
-        cawgn(&x[i],nstd);
-};
+#include <signal_generator/signal_generator.h>
 
 int main() {
     // Create sequence with 16 samples, repeated 10 times, starting after 30 Samples, padded with noise
