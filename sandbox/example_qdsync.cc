1f71fd03197ba2dc683f64823a7af77078d815be
F-L-X-S <felixschuelke@icloud.com>
2025-04-26 11:39:20 +0200

    Setup of qdsync for tf in single subcarrier

--- sandbox/example_qdsync.cc
+++ sandbox/example_qdsync.cc
@@ -1,90 +1,112 @@
+/**
+ * @file example_qdsync.cc
+ * @brief This example demonstrates the use of the Liquid qdsync algorithm for synchronization.
+ * A trainingfield is generated, that contains a repeating pattern of complex numbers. 
+ * The training field is then interpolated and inserted into a longer sequence where noise is added.
+ * Regarding an ofdm-signal, the SAMPLES_PER_SYMBOL and the training-field are always referring to a single subcarrier.
+ * 
+ * The liquid qdsync algorithm uses a cross-correlation a specific pattern to detect the start of the frame.
+ * We use the whole trainingfield containing repeating tf-symbols for the cross-correlation.
+ * 
+ */
+
 #include <iostream>
 #include <cmath>
 #include <complex>
+#include <cassert>
 #include <liquid/liquid.h>
 #include <correlation/auto_corr.h>
 #include <signal_generator/signal_generator.h>
 #include <matlab_export/matlab_export.h>
 
-#define NUM_SAMPLES 400
-#define STF_SYMBOL_LENGTH 16
-#define STF_SYMBOL_REPEAT 4
-#define STF_SYMBOL_START 30
+// Definition of the training-field (Pattern to be detected)
+#define TF_SYMBOL_LENGTH 16         // Number of Symbols a single training-field-symbol (pattern) contains
+#define TF_SYMBOL_REPEAT 10         // Number of times the training-field-symbol (pattern) is repeated
+#define TF_SYMBOL_START 30          // Start position of the repeated training-field-symbols (pattern) in the sequence
 
-#define OUTFILE "matlab/example_qdsync_out.m"
+// Definition of the receiver-settings 
+#define SAMPLES_PER_SYMBOL 2        // samples/symbol (interpolation factor for the training-field)
+#define FILTER_DELAY 7              // filter delay [symbols] (number of symbols, the filter has to wait before taking the first median value)
+#define NUM_SAMPLES 400             // Total Number of samples to be generated 
+
+// Output file in MATLAB-format to store results
+#define OUTFILE "matlab/example_qdsync_out.m" 
+
 
 // synchronization callback (return 0:continue, 1:reset)
-int callback(std::complex<float>* _buf, unsigned int _buf_len, void* _context){
-    std::vector<float> buffer;
+int callback(std::complex<float>* _buf, unsigned int _buf_len, void* buffer){
     for (unsigned int i = 0; i < _buf_len; ++i) {
-        buffer.push_back(_buf[i].real());  
-        buffer.push_back(_buf[i].imag()); 
+        static_cast<std::vector<std::complex<float>>*>(buffer)->push_back(_buf[i]);  
     }
-    MatlabExport(buffer, "buffer", OUTFILE);
+    return 0;
 }
 
 int main() {
     // ---------------------- Signal Generation ----------------------
+    // Check if the number of samples is sufficient to contain the interpolated training field
+    assert(NUM_SAMPLES > TF_SYMBOL_START + (TF_SYMBOL_LENGTH * TF_SYMBOL_REPEAT)*SAMPLES_PER_SYMBOL); 
     // Create sequence with 'NUM_SAMPLES' samples in total
-    unsigned int num_samples = NUM_SAMPLES;
-    std::complex<float> x[num_samples]; 
+    std::complex<float> x[NUM_SAMPLES]; 
 
-    // Generate a random pattern of complex numbers
-    std::complex<float> pattern[STF_SYMBOL_LENGTH];
-    for (unsigned int i = 0; i < STF_SYMBOL_LENGTH; i++) {
-        float re = (rand() % 2 ? 1.0f : -1.0f) * M_SQRT1_2;
-        float im = (rand() % 2 ? 1.0f : -1.0f) * M_SQRT1_2;
+    // Generate a pattern of sqrt(1/2)*(1+j) (example symbol transmitted in L-STF)
+    std::complex<float> pattern[TF_SYMBOL_LENGTH];
+    for (unsigned int i = 0; i < TF_SYMBOL_LENGTH; i++) {
+        float re = 1 * M_SQRT1_2;
+        float im = 1 * M_SQRT1_2;
         pattern[i] = std::complex<float>(re, im);
     }
 
-    // Generate the training field containing 'STF_SYMBOL_REPEAT' symbols, each of length 'STF_SYMBOL_LENGTH'
-    unsigned int tf_len = STF_SYMBOL_LENGTH * STF_SYMBOL_REPEAT;
+    // Generate the training field containing 'TF_SYMBOL_REPEAT' symbols, each of length 'TF_SYMBOL_LENGTH'
+    unsigned int tf_len = TF_SYMBOL_LENGTH * TF_SYMBOL_REPEAT;
     std::complex<float> tf[tf_len]; 
-    GenerateRepeatingSequence(pattern, STF_SYMBOL_LENGTH, STF_SYMBOL_REPEAT, tf);
+    GenerateRepeatingSequence(pattern, TF_SYMBOL_LENGTH, TF_SYMBOL_REPEAT, tf);
 
     // Interpolate the Training-field 
-    unsigned int k            =    2;   // samples/symbol
-    unsigned int m            =    7;   // filter delay [symbols]
+    firinterp_crcf interp = firinterp_crcf_create_prototype(LIQUID_FIRFILT_ARKAISER,SAMPLES_PER_SYMBOL,FILTER_DELAY,0.3f,0);
+    unsigned int tf_i_len = tf_len*SAMPLES_PER_SYMBOL+2;                                  // Length of the interpolated training field
+    std::complex<float> tf_i[tf_i_len];                                                 // Interpolated training field
 
-    firinterp_crcf interp = firinterp_crcf_create_prototype(LIQUID_FIRFILT_ARKAISER,k,m,0.3f,0);
-    std::complex<float> tf_i[tf_len];   // Interpolated training field
+    // Interpolate each tf-symbol by the factor 'SAMPLES_PER_SYMBOL' 
     for (unsigned int i = 0; i < tf_len; ++i) {
-        firinterp_crcf_execute(interp, tf[i], &tf_i[i*k]);
+        firinterp_crcf_execute(interp, tf[i], &tf_i[i*SAMPLES_PER_SYMBOL]);
     }
 
-    // Insert the interpolated training field into the longer sequence at the specified start position 'STF_SYMBOL_START'
-    InsertSequence(x, tf_i, STF_SYMBOL_START, k*tf_len);
+    // Insert the interpolated training field into the longer sequence at the specified start position 'TF_SYMBOL_START'
+    InsertSequence(x, tf_i, TF_SYMBOL_START, tf_i_len);
 
     // Add noise to the sequence
     float SNRdB = 20.0f; // signal-to-noise ratio (dB)
-    AddNoise(x, num_samples, SNRdB);
+    AddNoise(x, NUM_SAMPLES, SNRdB);
 
     // ----------------- Synchronization ----------------------
-    std::complex<float> rxx_results[num_samples];   // Autocorrelation results
-    float tau_results[num_samples];                 // fractional timing offset results
-    float dphi_results[num_samples];                // frequency offset estimate results
-    float phi_results[num_samples];                 // phase offset estimate results
+    std::vector<std::complex<float>> buffer;        // Buffer to store detected symbols 
+    std::complex<float> rxy_results[NUM_SAMPLES];   // Crosscorrelation results
+    float tau_results[NUM_SAMPLES];                 // fractional timing offset results
+    float dphi_results[NUM_SAMPLES];                // frequency offset estimate results
+    float phi_results[NUM_SAMPLES];                 // phase offset estimate results
 
-    qdsync_cccf sync = qdsync_cccf_create_linear(pattern, STF_SYMBOL_LENGTH, LIQUID_FIRFILT_ARKAISER, k, m, 0.3f, callback, NULL);
+    qdsync_cccf sync = qdsync_cccf_create_linear(tf, tf_len, LIQUID_FIRFILT_ARKAISER, SAMPLES_PER_SYMBOL, FILTER_DELAY, 0.3f, callback, &buffer);
 
-    for (unsigned int i = 0; i < num_samples; ++i) {
+    for (unsigned int i = 0; i < NUM_SAMPLES; ++i) {
         // Process synchronization for each single sample 
-        qdsync_cccf_execute(sync, &x[i], STF_SYMBOL_LENGTH);
+        qdsync_cccf_execute(sync, &x[i], TF_SYMBOL_LENGTH);
 
         // Get the results
-        rxx_results[i] = qdsync_cccf_get_rxy(sync);
+        rxy_results[i] = qdsync_cccf_get_rxy(sync);
         tau_results[i] = qdsync_cccf_get_tau(sync);
         dphi_results[i] = qdsync_cccf_get_dphi(sync);
         phi_results[i] = qdsync_cccf_get_phi(sync);
     }
+    firinterp_crcf_destroy(interp);
     qdsync_cccf_destroy(sync);
 
     // ----------------- MATLAB-compatible output in terminal ----------------------
-    MatlabExport(std::vector<std::complex<float>>(x, x + num_samples), "x", OUTFILE);
-    MatlabExport(std::vector<std::complex<float>>(rxx_results, rxx_results + num_samples), "rxx", OUTFILE);
-    MatlabExport(std::vector<float>(tau_results, tau_results + num_samples), "tau", OUTFILE);
-    MatlabExport(std::vector<float>(dphi_results, dphi_results + num_samples), "dphi", OUTFILE);
-    MatlabExport(std::vector<float>(phi_results, phi_results + num_samples), "phi", OUTFILE);
+    MatlabExport(std::vector<std::complex<float>>(x, x + NUM_SAMPLES), "x", OUTFILE);
+    MatlabExport(buffer, "buffer", OUTFILE);
+    MatlabExport(std::vector<std::complex<float>>(rxy_results, rxy_results + NUM_SAMPLES), "rxy", OUTFILE);
+    MatlabExport(std::vector<float>(tau_results, tau_results + NUM_SAMPLES), "tau", OUTFILE);
+    MatlabExport(std::vector<float>(dphi_results, dphi_results + NUM_SAMPLES), "dphi", OUTFILE);
+    MatlabExport(std::vector<float>(phi_results, phi_results + NUM_SAMPLES), "phi", OUTFILE);
     
     return 0;
 }
