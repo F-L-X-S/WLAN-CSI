4b3f46321dd1e18ad1a767915a092e6543ce6d94
F-L-X-S <felixschuelke@icloud.com>
2025-07-17 09:10:53 +0200

    Sample Block Queue between RX- and Sync-worker

---
+++ include/multi_rx/multi_rx.h
@@ -0,0 +1,279 @@
+/**
+ * @file multi_rx.h
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * 
+ * @brief 
+ * @version 0.1
+ * @date 2025-07-11
+ * 
+ * 
+ */
+
+#ifndef MULTIRX_H
+#define MULTIRX_H
+
+#include <uhd/usrp/multi_usrp.hpp>    
+              
+#include <vector>                     
+#include <complex>      
+
+#include <queue>       
+#include <mutex>                    
+#include <condition_variable>         
+#include <string>              
+#include <atomic>                     
+
+#include <multisync/multisync.h>
+#include <zmq_socket/zmq_socket.h>
+#include <matlab_export/matlab_export.h>
+
+// Typedefs
+using RxSample_t = std::complex<float>;   // Received samples type
+
+struct RxSampleBlock_t
+{
+    std::vector<RxSample_t> samples;
+    uhd::time_spec_t timestamp;
+};
+
+struct RxSamplesQueue_t
+{
+    std::queue<RxSampleBlock_t> queue;
+    std::mutex mtx;
+    std::condition_variable cv;
+};
+
+struct Cfr_t {
+    std::vector<std::complex<float>> cfr;               // Channel Frequency Response
+    uhd::time_spec_t  timestamp;                        // Timestamp of the CFR
+    unsigned int channel;                               // Channel index
+};
+struct CfrQueue_t
+{
+    std::queue<Cfr_t> queue;
+    std::mutex mtx;
+    std::condition_variable cv;
+};
+
+
+// RX-Worker receives samples from the USRP and pushes them into a thread-safe queue
+template <std::size_t buffer_size>
+void rx_worker( uhd::rx_streamer::sptr rx_stream,
+                RxSamplesQueue_t& q,
+                std::atomic<bool>& stop_signal_called) {
+    uhd::rx_metadata_t md;
+    std::vector<RxSample_t> buff(buffer_size);
+
+    while (!stop_signal_called.load()) {
+        size_t n_rx = rx_stream->recv(&buff.front(), buff.size(), md, 1.0);
+        RxSampleBlock_t sample_block;
+        sample_block.samples.assign(buff.begin(), buff.begin() + n_rx);
+        sample_block.timestamp = md.time_spec;
+        {
+            std::lock_guard<std::mutex> lock(q.mtx);
+            q.queue.push(std::move(sample_block));
+        }
+        q.cv.notify_one();
+    }
+}
+
+// Sync-Worker processes queued samples from RX-workers and pushes the resulting CFRs into a thread-safe queue
+template <std::size_t num_channels, typename syncronizer_type, typename cb_data_type>
+void sync_worker(   std::array<resamp_crcf, num_channels>& resamplers,
+                    syncronizer_type& ms,
+                    std::array<cb_data_type, num_channels>& cb_data,
+                    std::array<RxSamplesQueue_t, num_channels>& rx_queues,
+                    CfrQueue_t& cfr_queue,
+                    std::atomic<bool>& stop_signal_called
+                ) {
+
+    std::vector<RxSampleBlock_t> sample_blocks;
+    std::vector<std::complex<float>> rx_sample(1);
+    Cfr_t cfr;                                                        
+    unsigned int i, j, num_written;
+
+    while (!stop_signal_called.load()) {
+        // Process channels
+        for (i = 0; i < num_channels; ++i) {
+                // Clear samples and Callback-data
+                sample_blocks.clear();
+                cb_data[i].buffer.clear();  
+
+                // Process channel queue 
+                std::unique_lock<std::mutex> lock_rx(rx_queues[i].mtx);
+                rx_queues[i].cv.wait(lock_rx, [&rx_queues, i, &stop_signal_called] { 
+                    return !rx_queues[i].queue.empty() || stop_signal_called.load(); 
+                });
+
+                while (!rx_queues[i].queue.empty()) {
+                    sample_blocks.push_back(std::move(rx_queues[i].queue.front()));
+                    rx_queues[i].queue.pop();
+                }
+
+                // Detect Packets 
+                for (j = 0; j < sample_blocks.size(); ++j) {
+                        // Process all Samples in Block 
+                        for (unsigned int k = 0; k < sample_blocks[j].samples.size(); ++k) { 
+                            // Resample to original carrier frequency
+                            resamp_crcf_execute(    resamplers[i], 
+                                                    sample_blocks[j].samples[k],
+                                                    &rx_sample[0], &num_written);  
+                                
+                            // Execute Synchronizer for channel i 
+                            ms.Execute(i, &rx_sample);          
+                                
+                            // Check, if callback-data was updated by synchronizer
+                            if (cb_data[i].buffer.size()){                         
+                                cfr.cfr.assign(64, std::complex<float>(0.0f, 0.0f));                    // initialize CFR Buffer for 64 FFT points
+                                ms.GetCfr(i, &cfr.cfr, 64);                                             // Write cfr to callback data
+                                cfr.timestamp = sample_blocks[j].timestamp;                             // Update timestamp
+                                cfr.channel = i;                                                        // Set channel index
+
+                                // Push CFR to queue
+                                {
+                                    std::lock_guard<std::mutex> lock_cfr(cfr_queue.mtx);
+                                    cfr_queue.queue.push(std::move(cfr));
+                                }
+                                cfr_queue.cv.notify_one();
+                                std::cout << "Captured CFR for channel "<< i <<"!" << std::endl;
+                                break;
+                            };
+                        };
+                    };
+                };
+
+
+        // Synchronize NCOs of all channels to the average NCO frequency and phase
+        ms.SynchronizeNcos();
+    }
+}
+
+// Export-Worker processes queued CFRs from Sync-workers and either discards them or exports them to a ZMQ socket
+template <std::size_t num_channels>
+void export_worker( CfrQueue_t& cfr_queue, 
+                    uhd::time_spec_t max_age,
+                    ZmqSender& sender,
+                    MatlabExport& m_file,
+                    std::atomic<bool>& stop_signal_called) {
+
+    // Queued CFRs of all channels and all times 
+    std::vector<Cfr_t> cfr_buffer;         
+
+    // Sorted and time-matched CFRs of all channels
+    std::vector<std::vector<std::complex<float>>> cfr_group(num_channels);
+
+    unsigned int i;
+    while (!stop_signal_called.load()) {
+        // Move cfr queue to buffer
+        std::unique_lock<std::mutex> lock_cfr(cfr_queue.mtx);
+        cfr_queue.cv.wait(lock_cfr, [&cfr_queue, &stop_signal_called] { 
+            return !cfr_queue.queue.empty() || stop_signal_called.load(); 
+        });
+
+        if (!cfr_queue.queue.empty()) {
+            cfr_buffer.push_back(std::move(cfr_queue.queue.front()));
+            cfr_queue.queue.pop();
+        }
+
+        // Sort CFRs by timestamp
+        std::sort(cfr_buffer.begin(), cfr_buffer.end(),
+            [](const Cfr_t& a, const Cfr_t& b) {
+                return a.timestamp < b.timestamp;
+            });
+
+        // Find a group of CFRs from all channels within the max_age window
+        unsigned int i;
+        for (i = 0; i < cfr_buffer.size(); ++i) {
+            std::vector<const Cfr_t*> group(num_channels, nullptr); // Group of CFRs from each channel
+            const auto& base = cfr_buffer[i];                       // Add initial CFR to group
+            group[base.channel] = &base;
+
+            // Find all CFRs around base within the max_age window
+            for (size_t j = i + 1; j < cfr_buffer.size(); ++j) {
+                // Next timestamp out of range -> no group existing for this base-CFR
+                if ((cfr_buffer[j].timestamp - base.timestamp) > max_age)
+                    break;
+                // Found CFR for another channel within the max_age window
+                if (!group[cfr_buffer[j].channel])
+                    group[cfr_buffer[j].channel] = &cfr_buffer[j];
+            }
+
+            // Check if group is complete (all channels have a CFR)
+            bool complete = true;
+            for (auto ptr : group) {
+                if (!ptr) { complete = false; break; }
+            }
+
+            // Export if a complete group was found
+            if (complete) {
+                // Reset the CFR group
+                cfr_group = std::vector<std::vector<std::complex<float>>>(num_channels);
+                // Prepare CFRs sorted by channel
+                for (size_t ch = 0; ch < num_channels; ++ch)
+                    cfr_group[ch] = group[ch]->cfr;
+
+                //----------------- ZMQ Export ----------------------
+                sender.send(cfr_group);
+                std::cout << "Exported CFR!\n"<< std::endl;
+
+                // Clear the buffer up to the current index
+                cfr_buffer.erase(cfr_buffer.begin(), cfr_buffer.end());
+
+
+                //----------------- MATLAB Export ----------------------
+                // Export CFRs to MATLAB file
+                for (unsigned int i = 0; i < num_channels; ++i) {
+                    std::string suffix = std::to_string(i);
+                    m_file.Add(cfr_group[i], "cfr_" + suffix);
+                }
+
+                // Add combined plot-commands to the MATLAB file
+                std::stringstream matlab_cmd;
+
+                matlab_cmd << "figure;";
+                // CFR Magnitude
+                matlab_cmd << "subplot(2,1,1); hold on;";
+                for (unsigned int i = 0; i < num_channels; ++i) {
+                    std::string suffix = std::to_string(i);
+                    matlab_cmd << "plot(abs(cfr_" << suffix << "), 'DisplayName', 'RX-Channel " << suffix << "');";
+                }
+                matlab_cmd << "title('Channel Frequency Response Gain'); legend; grid on;";
+                matlab_cmd << std::endl;
+
+                // CFR Phase
+                matlab_cmd << "subplot(2,1,2); hold on;";
+                for (unsigned int i = 0; i < num_channels; ++i) {
+                    std::string suffix = std::to_string(i);
+                    matlab_cmd << "plot(angle(cfr_" << suffix << "), 'DisplayName', 'RX-Channel " << suffix << "');";
+                }
+                matlab_cmd << "title('Channel Frequency Response Phase'); legend; grid on;";
+                matlab_cmd << std::endl;
+
+                // CFR in Complex 
+                matlab_cmd << "figure;";
+                for (unsigned int i = 0; i < num_channels; ++i) {
+                    std::string suffix = std::to_string(i);
+                    matlab_cmd << "plot(real(cfr_" << suffix << "), imag(cfr_" << suffix
+                            << "), '.', 'DisplayName', 'RX-Channel " << suffix << "'); hold on;";
+                }
+                matlab_cmd << "title('CFR'); xlabel('Real'); ylabel('Imag'); axis equal; legend; grid on;";
+                matlab_cmd << std::endl;
+
+                // Add the complete command string to MATLAB export
+                m_file.Add(matlab_cmd.str());
+
+                // Break queue processing 
+                break;
+            }
+        }
+
+        // Clear all processed CFRs from the buffer, keep min. one CFR for each channel
+        if (cfr_buffer.size() > num_channels){
+            int rest = static_cast<int>(cfr_buffer.size()) - static_cast<int>(i) - 1;
+            cfr_buffer.erase(cfr_buffer.begin(), cfr_buffer.end() - (rest>static_cast<int>(num_channels) ? rest : static_cast<int>(num_channels)));
+        };
+    }
+
+}
+
+#endif // MULTIRX_H
---
+++ sandbox/example_usrp_music.cc
@@ -0,0 +1,220 @@
+/**
+ * @file example_usrp_music.cc
+ * @author Felix Schuelke 
+ * @brief USRP integartion based on Ettus example project https://kb.ettus.com/Getting_Started_with_UHD_and_C%2B%2B
+ * @version 0.1
+ * @date 2025-07-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <uhd/utils/thread_priority.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/exception.hpp>
+#include <uhd/types/tune_request.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+#include <boost/thread.hpp>
+#include <iostream>
+#include <csignal>
+#include <atomic>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+#include <matlab_export/matlab_export.h>
+#include <multisync/multisync.h>
+#include <multi_rx/multi_rx.h>
+#include <zmq_socket/zmq_socket.h>
+
+#define NUM_CHANNELS 2                              // Number of Channels (USRP-devices)   
+#define OUTFILE "./matlab/example_usrp_music.m"     // Output file in MATLAB-format to store results
+#define EXPORT_INTERFACE 'tcp://localhost:5555'     // Interface for zmq socket
+
+using Sync_t = MultiSync<ofdmframesync>;
+
+// Signal handler to stop by keaboard interrupt
+std::atomic<bool> stop_signal_called(false);
+void sig_int_handler(int) {
+    stop_signal_called.store(true);
+}
+
+// custom data type to pass to callback function
+struct callback_data {
+    std::vector<std::complex<float>> buffer;        // Buffer to store detected symbols 
+};
+
+// callback function
+//  _X          : array of received subcarrier samples [size: _M x 1]
+//  _p          : subcarrier allocation array [size: _M x 1]
+//  _M          : number of subcarriers
+//  _userdata   : user-defined data pointer
+static int callback(std::complex<float>* _X, unsigned char * _p, unsigned int _M, void * _cb_data){
+    // Add symbols from all subcarriers to buffer 
+    for (unsigned int i = 0; i < _M; ++i) {
+        // ignore 'null' and 'pilot' subcarriers
+        if (_p[i] != OFDMFRAME_SCTYPE_DATA)
+            continue;
+        static_cast<callback_data*>(_cb_data)->buffer.push_back(_X[i]);  
+        std::cout << boost::format("Callback!");
+    }
+return 0;
+}
+
+// Main function 
+int UHD_SAFE_MAIN(int argc, char *argv[]) {
+    uhd::set_thread_priority_safe();
+    std::signal(SIGINT, &sig_int_handler);
+
+   // Receiver settings 
+    unsigned long int ADC_RATE = 100e6;
+    double rx_rate = 25e6f;
+    // NOTE : the sample rate computation MUST be in double precision so
+    //        that the UHD can compute its decimation rate properly
+    unsigned int decim_rate = (unsigned int)(ADC_RATE / rx_rate);
+    // ensure multiple of 2
+    decim_rate = (decim_rate >> 1) << 1;
+    // compute usrp sampling rate
+    double usrp_rx_rate = ADC_RATE / (float)decim_rate;
+    // compute the resampling rate (20MHz to 40 MHz = 0.5)
+    float rx_resamp_rate = 0.5*rx_rate / usrp_rx_rate;
+
+    //create USRP devices
+    std::string device_args_0("addr=192.168.10.3");
+    std::string device_args_1("addr=192.168.168.2");
+    uhd::usrp::multi_usrp::sptr usrp_0 = uhd::usrp::multi_usrp::make(device_args_0);
+    uhd::usrp::multi_usrp::sptr usrp_1 = uhd::usrp::multi_usrp::make(device_args_1);
+
+    // Lock mboard clocks
+    usrp_0->set_clock_source("internal", 0);  // internal clock source for device 0 
+    usrp_1->set_time_source("mimo", 0);       // mimo clock source for device 1
+    usrp_1->set_clock_source("mimo", 0);      // mimo clock source for device 1
+    usrp_0->set_time_now(uhd::time_spec_t(0.0), 0);   // set time for device 0 
+    usrp_1->set_time_now(uhd::time_spec_t(0.0), 0);   // set time for device 1
+
+    std::cout << boost::format("Device 1 Clock-src: %s") % usrp_1->get_clock_source(0) << std::endl << std::endl;
+    std::cout << boost::format("Device 1 Time-src: %s") % usrp_1->get_time_source(0) << std::endl << std::endl;
+    
+    //sleep a bit while the slave locks its time to the master
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+
+    //always select the subdevice first, the channel mapping affects the other settings
+    usrp_0->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("A:0"), 0);  //set the device 0 to use the A RX frontend (RX channel 0)
+    usrp_1->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("A:0"), 0);  //set the device 1 to use the A RX frontend (RX channel 0)
+ 
+    // set sample rate
+    usrp_0->set_rx_rate(usrp_rx_rate, uhd::usrp::multi_usrp::ALL_MBOARDS);
+    usrp_1->set_rx_rate(usrp_rx_rate, uhd::usrp::multi_usrp::ALL_MBOARDS);
+    std::cout << boost::format("Required USRP RX Rate: %f Msps...") % (usrp_rx_rate / 1e6) << std::endl;
+    std::cout << boost::format("Device 0 RX Rate: %f Msps...") % (usrp_0->get_rx_rate(0) / 1e6) << std::endl;
+    std::cout << boost::format("Device 1 RX Rate: %f Msps...") % (usrp_1->get_rx_rate(0) / 1e6) << std::endl;
+    std::cout << boost::format("Resampling-Rate: %f Msps...") % (rx_resamp_rate) << std::endl;
+
+    // set freq
+    uhd::tune_request_t tune_request(2220e6, 227e6);  // create a tune request with the desired frequency and local oscillator offset
+    std::cout << boost::format("Tune Policy: %f") % (tune_request.rf_freq_policy) << std::endl;
+    usrp_0->set_rx_freq(tune_request, 0);
+    usrp_1->set_rx_freq(tune_request, 0);
+    std::cout << boost::format("Device 0 RX Freq: %f MHz...") % (usrp_0->get_rx_freq(0) / 1e6) << std::endl;
+    std::cout << boost::format("Device 1 RX Freq: %f MHz...") % (usrp_1->get_rx_freq(0) / 1e6) << std::endl;
+
+    // set the rf gain
+    usrp_0->set_rx_gain(30, 0);
+    usrp_1->set_rx_gain(30, 0);
+    std::cout << boost::format("Device 0 RX Gain: %f dB...") % usrp_0->get_rx_gain(0) << std::endl;
+    std::cout << boost::format("Device 1 RX Gain: %f dB...") % usrp_1->get_rx_gain(0) << std::endl;
+
+    // Print Bandwidth 
+    std::cout << boost::format("Device 0 RX Bandwidth: %f MHz...") % (usrp_0->get_rx_bandwidth(0) / 1e6) << std::endl;
+    std::cout << boost::format("Device 1 RX Bandwidth: %f MHz...") % (usrp_1->get_rx_bandwidth(0) / 1e6) << std::endl << std::endl;
+
+    // set the antenna
+    usrp_0->set_rx_antenna("RX2", 0);
+    usrp_1->set_rx_antenna("RX2", 0);
+    
+    // Receive stream confguration
+    uhd::stream_args_t stream_args("fc32");                                                 // convert internal sc16 to complex float 32
+    stream_args.args["recv_buff_size"] = "100000000"; // 100MB Buffer
+    uhd::rx_streamer::sptr rx_stream_0 = usrp_0->get_rx_stream(stream_args);                // create receive streams 
+    uhd::rx_streamer::sptr rx_stream_1 = usrp_1->get_rx_stream(stream_args);                // cretae a receive stream 
+    size_t max_samps = rx_stream_0->get_max_num_samps();  
+
+    // Resamplers
+    std::array<resamp_crcf, 2> resamplers = {
+        resamp_crcf_create(rx_resamp_rate, 12, 0.45f, 60.0f, 32),
+        resamp_crcf_create(rx_resamp_rate, 12, 0.45f, 60.0f, 32)
+    };
+     
+    // Thread-safe queues 
+    std::array<RxSamplesQueue_t, 2> rx_queues;
+    CfrQueue_t cfr_queue;
+
+    // callback data
+    std::array<callback_data, NUM_CHANNELS> cb_data;
+    void* userdata[NUM_CHANNELS];
+
+    // Array of Pointers to CB-Data 
+    for (unsigned int i = 0; i < NUM_CHANNELS; ++i)
+        userdata[i] = &cb_data[i];
+        
+    // Create multi frame synchronizer
+    unsigned int M           = 64;      // number of subcarriers 
+    unsigned int cp_len      = 16;      // cyclic prefix length (800ns for 20MHz => 16 Sample)
+    unsigned int taper_len   = 4;       // window taper length 
+    unsigned char p[M];                 // subcarrier allocation array
+    ofdmframe_init_default_sctype(M, p);
+    Sync_t ms(NUM_CHANNELS, {M, cp_len, taper_len, p}, callback, userdata);
+
+    // ZMQ socket for data export 
+    ZmqSender sender("tcp://*:5555");
+
+    // Matlab Export destination file
+    MatlabExport m_file(OUTFILE);
+
+    // Configure stream command
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+    stream_cmd.num_samps = max_samps;                                                   // number of samples to receive per frame
+    stream_cmd.stream_now = false;  
+    double seconds_in_future = 5.0;            
+    stream_cmd.time_spec = usrp_0->get_time_now() + uhd::time_spec_t(seconds_in_future);  // set the time spec to start receiving in the future
+
+    // Start USRPs streaming
+    usrp_0->issue_stream_cmd(stream_cmd); 
+    usrp_1->issue_stream_cmd(stream_cmd); 
+
+    // Start receiving...
+    std::thread t0(rx_worker<4096>, rx_stream_0, std::ref(rx_queues[0]), std::ref(stop_signal_called));
+    std::thread t1(rx_worker<4096>, rx_stream_1, std::ref(rx_queues[1]), std::ref(stop_signal_called));
+    std::thread t2(sync_worker<NUM_CHANNELS, Sync_t, callback_data>, 
+        std::ref(resamplers), std::ref(ms), 
+        std::ref(cb_data), std::ref(rx_queues),
+        std::ref(cfr_queue), std::ref(stop_signal_called));
+    std::thread t3(export_worker<NUM_CHANNELS>, std::ref(cfr_queue), double(1), std::ref(sender), std::ref(m_file), std::ref(stop_signal_called));
+
+    // Let it run for a while...
+    std::this_thread::sleep_for(std::chrono::seconds(500));
+
+    // Signal stop
+    usrp_0->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+    usrp_1->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+
+    rx_queues[0].cv.notify_all();
+    rx_queues[1].cv.notify_all();
+    cfr_queue.cv.notify_all();
+
+    t0.join();
+    t1.join();
+    t2.join();
+    t3.join();
+
+    for (auto& r : resamplers) {
+    resamp_crcf_destroy(r);
+    }
+
+    std::cout << "Stopped receiving...\n" << std::endl;
+
+return EXIT_SUCCESS;
+}
